9ceaee73dd01c29aeb088d8f27c026fd3adb0cf0
admin <kapanski@mail.ru>
2021-09-06 12:39:40 +0300

    test

--- cluster.py
+++ cluster.py
@@ -1,3 +1,5 @@
+import sys
+
 import openpyxl as xl
 from openpyxl.chart import Reference,  LineChart
 # import seaborn as sns; sns.set()
@@ -12,10 +14,9 @@ pd.set_option('display.max_columns', 7)
 
 class Cluster():
 
-    def __init__(self, df_initial, n_cluster, file_name):
+    def __init__(self, df_initial, n_cluster):
         self.df_initial=df_initial
         self.n_cluster=n_cluster
-        self.file_name=file_name
         self.df_new = None
         self.df_cluster_for_plot = None
         self.df_agg = None
@@ -33,23 +34,76 @@ class Cluster():
         sheet_1.add_chart(chart, 'I2')
         workbook.save(f'Результаты расчета/{file_name}')
 
+    def _change(self, df, name):
+        df['RollT'] = np.roll(df, 1)
+        df['Шаг времени, ч'] = pd.to_timedelta(df.iloc[1, 0] - df.iloc[1, 1]).total_seconds() / 3600
+        df['Минута'] = df[name].apply(lambda x: x.minute)
+        df['Час'] = df.apply(lambda x: round(x[name].hour + x['Минута'] / 60, 1), axis=1)
+        df.drop(columns = ['RollT', 'Минута'], inplace=True)
+
+        return df
+
     def fun(self):
         df = self.df_initial.dropna()
+        print(df.dtypes)
         name_list = df.columns
         if len(name_list) > 2:
             return print('Количество переменных должно быть не более двух.')
-        elif df.iloc[:, 0].dtypes == 'datetime64[ns]':
-            df['RollT'] = np.roll(df['Дата'], 1)
-            df['dT, ч'] = pd.to_timedelta(df.iloc[1, 0] - df.iloc[1, 2]).total_seconds() / 3600
-            df = df.drop(columns=['RollT'])
-            df['Минута'] = df['Дата'].apply(lambda x: x.minute)
-            df['Час'] = df.apply(lambda x: x['Дата'].hour + x['Минута'] / 60, axis=1)
+        elif df.iloc[:, 0].dtypes == 'datetime64[ns]' or df.iloc[:, 1].dtypes == 'datetime64[ns]':
+            name_x = df.columns[0]
+            name_y = df.columns[1]
+            X = pd.DataFrame().to_numpy()
+            if df[name_x].dtypes == 'datetime64[ns]':
+                df_x = self._change(df=df[[name_x]], name=name_x)
+                df_y = df[[name_y]]
+                df = pd.concat([df_x, df_y], axis=1)
+                X = df.iloc[:, [2, 3]].to_numpy()
+            elif df[name_y].dtypes == 'datetime64[ns]':
+                df_y = self._change(df=df[[name_y]], name=name_y)
+                df_x = df[[name_x]]
+                df = pd.concat([df_y, df_x], axis=1)
+                X = df.iloc[:, [3, 2]].to_numpy()
 
-            if df['dT, ч'].mean() <= 24:
+            kmeans = KMeans(n_clusters=self.n_cluster)
+            kmeans.fit_predict(X)
+            #Класстер сместили на 1 для удосбтва восприятия. Начинается не с нуля
+            y_kmeans = np.array(list(map(lambda x: x+1, kmeans.predict(X))))
+            # y_kmeans = kmeans.predict(X)
+
+            predict = pd.DataFrame(y_kmeans, columns=['Номер кластера'])
+            df_result = pd.concat([df, predict], axis=1)
+            df_result = df_result.set_index('Номер кластера')
+            centers = kmeans.cluster_centers_
+            df_center = pd.DataFrame(centers, columns=[f'Центроида: {name_x}',
+                                                       f'Центроида: {name_y}'])
+            clusters_list = [i+1 for i in range(self.n_cluster)]
+            df_clust = pd.DataFrame(clusters_list, columns=['Номер кластера'])
+            df_center = pd.concat([round(df_center, 1), df_clust], axis=1).set_index('Номер кластера')
+
+            df_new = df_result.join(df_center).reset_index()
+            df_new.sort_values(by=['Час'], inplace=True)
+            df_new['Порядковый номер'] = np.arange(len(df_new))
+
+
+            self.df_new = df_new.iloc[:, [1, 2, 3, 0, 6, 4, 5]]
+            self.df_cluster_for_plot = self.df_new.iloc[:, [2, 4, 2, 5]]
+            print(self.df_cluster_for_plot.columns[0])
+
+
+            if df['Шаг времени, ч'].mean() >= 24:
+
+                pass
+
+
+
+
+
                 X = df.iloc[:, [1, 4]].to_numpy()
+
                 kmeans = KMeans(n_clusters=self.n_cluster)
                 kmeans.fit_predict(X)
                 y_kmeans = kmeans.predict(X)
+
                 predict = pd.DataFrame(y_kmeans, columns=['Номер кластера'])
 
                 df_result = pd.concat([df, predict], axis=1)
@@ -73,10 +127,9 @@ class Cluster():
                      f'Центроида: {name_list[0]}': 'mean'}).reset_index()
                 self.df_agg = df_agg.rename(columns={'Порядковый номер': 'Количество значений попавших в диапазон'})
 
-            if df['dT, ч'].mean() > 24:
-                pass
-
         elif len(df.columns) == 1:
+
+            df.sort_values(by=[df.iloc[:, [0]].columns[0]], inplace=True, ascending=False)
             df['Уникальный номер'] = np.arange(len(df))
 
             X = df.iloc[:, [0, 1]].to_numpy()
@@ -94,7 +147,7 @@ class Cluster():
             df_clust = pd.DataFrame(clusters_list, columns=['Номер кластера'])
             df_center = pd.concat([round(df_center, 1), df_clust], axis=1).set_index('Номер кластера')
             df_new = df_result.join(df_center).reset_index()
-            df_new.sort_values(by=[f'Центроида: {df.iloc[:, [0]].columns[0]}'], inplace=True)
+            df_new.sort_values(by=[f'Центроида: {df.iloc[:, [0]].columns[0]}'], inplace=True,  ascending=False )
             df_new['Порядковый номер'] = np.arange(len(df_new))
             self.df_new = df_new.iloc[:, [0, 1, 5, 3]]
             self.df_cluster_for_plot = self.df_new.iloc[:, [2, 1, 2, 3]]
@@ -122,7 +175,7 @@ class Cluster():
                 df_clust = pd.DataFrame(clusters_list, columns=['Номер кластера'])
                 df_center = pd.concat([round(df_center, 1), df_clust], axis=1).set_index('Номер кластера')
                 df_new = df_result.join(df_center).reset_index()
-                df_new.sort_values(by=[f'Центроида: {name_list[1]}'], inplace=True)
+                df_new.sort_values(by=[f'Центроида: {name_list[1]}'], inplace=True, ascending=False)
                 df_new['Порядковый номер'] = np.arange(len(df_new))
 
 
@@ -137,14 +190,19 @@ class Cluster():
                print('Что-то пошло не так.')
 
 
-# path = 'd:\Синхронизация\++5 ПРОГРАММИРОВАНИЕ\Phaton\Мои Script на Python\Кластеризация streamlit\Анализируемая статистика.xlsx'
-# df = pd.read_excel(path)
-# print(df.dtypes)
-#
-# f = Cluster(df_initial=df, n_cluster=5, file_name='d')
-# f.fun()
-# # print(f.df_new)
-# print(f.df_cluster_for_plot)
+path = 'd:\РАБОТА, НАУКА И ПРОЧЕЕ ВАЖНОЕ!\++РАБОТА\++5 ПРОГРАММИРОВАНИЕ\Phaton\Мои Script на Python\Кластеризация streamlit\всякое\Анализируемая статистика.xlsx'
+
+df = pd.read_excel(path)
+
+
+f = Cluster(df_initial=df, n_cluster=5)
+f.fun()
+df_new = f.df_cluster_for_plot
+
+df_new.to_excel('gh.xlsx')
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
+
 import streamlit as st
 import pandas as pd
 import cluster as cl
+import numpy as np
 import base64
 from io import BytesIO
 import plotly.graph_objs as go
@@ -23,6 +26,7 @@ def write_to_excel(df_initial, df_new, df_cluster_for_plot, df_agg):
 
 
 # Функция позволяющая сделать download на выгрузку данных расчета
+@st.cache
 def get_table_download_link(df_initial, df_new, df_cluster_for_plot, df_agg):
     """Generates a link allowing the data in a given panda dataframe to be downloaded
     in:  dataframe
@@ -33,25 +37,97 @@ def get_table_download_link(df_initial, df_new, df_cluster_for_plot, df_agg):
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="Result_cluster.xlsx">' \
            f'Скачать xlsx файл результата</a>'  # decode b'abc' => abc
 
+# Кэшируем функцию создания кластеризатора
+@st.cache(allow_output_mutation=True)
+def create_cluster(df_initial, n_cluster):
+    cluster = cl.Cluster(df_initial=df_initial, n_cluster=n_cluster)
+    cluster.fun()
+    return cluster
+
 
 def app():
     st.title('Кластеризация')
     uploaded_file = st.file_uploader('Загрузите или перетащите файл Excel')
     try:
-        df_initial = pd.read_excel(uploaded_file)
-        st.subheader('Загруженные данные')
-        st.write(df_initial)
-    except Exception:
-        st.subheader('Файл еще не загружен.')
+        # Читаем вель эксель файл
+        xls = pd.ExcelFile(uploaded_file)
+    except Exception as err:
+        st.subheader('Excel файл еще не загружен.')
+        st.text(err)
     else:
         try:
+            st.sidebar.title('Настройки')
+            # Смотрим какие есть листы и загружаем в селект бокс
+            st.sidebar.subheader('Параметры рабочего листа файла')
+            selected_sheet_names = st.sidebar.selectbox('Выберите лист загруженного файла', (xls.sheet_names))
+            st.sidebar.write(f'Выбран лист: {selected_sheet_names}')
+
+            # Настройка боковой панели
+            st.sidebar.subheader('Наименование переменных кластеризации')
+            df_initial = pd.read_excel(uploaded_file, sheet_name=selected_sheet_names)
+            st.subheader('Фрагмент исходных данные')
+            st.write(df_initial.head(5))
+
+            df_initial['Количество наблюдений'] = ''
+
+            # Наименование переменных
+            selected_y = st.sidebar.selectbox('Выберите переменную Y', (df_initial.columns))
+            selected_x = st.sidebar.selectbox('Выберите переменную X', (df_initial.columns))
+
+            df_y = df_initial[[selected_y]]
+            name_y = selected_y + '(Y)'
+            df_y = df_y.rename(columns={selected_y: name_y})
+
+            name_x = selected_x + '(X)'
+            df_x = df_initial[[selected_x]]
+            df_x = df_x.rename(columns={selected_x: name_x})
+
+            name_val_x = 'Количество наблюдений(X)'
+            name_val_y = 'Количество наблюдений(Y)'
+
+            st.subheader('Фрагмент выбранных данных кластеризации')
+
+            if (name_y == name_val_y) and (name_x == name_val_x):
+                df_y[name_y] = np.arange(len(df_initial))
+                df_x[name_x] = np.arange(len(df_initial))
+                st.text('Сортирванные исходные данные в порядке убывания')
+            elif name_y == name_val_y:
+                df_x.sort_values(by=name_x, inplace=True, ascending=False)
+                df_x = df_x.reset_index(drop=True)
+                df_y[name_y] = np.arange(len(df_initial))
+                st.text('Сортирванные исходные данные в порядке убывания')
+            elif name_x == name_val_x:
+                df_y.sort_values(by=name_y, inplace=True, ascending=False)
+                df_y = df_y.reset_index(drop=True)
+                df_x[name_x] = np.arange(len(df_initial))
+                st.text('Сортирванные исходные данные в порядке убывания')
+
+            # Итоговый датафрейм
+            df = pd.concat([df_y,df_x], axis=1)
+
+
+            # df.sort_values(by=[selected_x1], inplace=True)
+            # df_initial['Количество наблюдений'] = np.arange(len(df_initial))
+
+            st.write(df.head(5))
+
+            # Ввод количества кластеров
             st.sidebar.subheader('Ввод количества кластеров')
-            n_cluster = int(st.sidebar.number_input('Поле ввода количества кластеров',value=5))
-            cluster = cl.Cluster(df_initial=df_initial, n_cluster= n_cluster, file_name ='Результаты кластеризации')
-            cluster.fun()
+            n_cluster = int(st.sidebar.number_input('Поле ввода количества кластеров', value=5))
+            # sys.exit()
+
+
+
+
+
+
+            cluster = create_cluster(df_initial=df, n_cluster=n_cluster)
+
             df_new = cluster.df_new
             st.subheader('Результат кластеризации')
             st.write(df_new)
+
+
             st.subheader('Графическая интерпритация')
             df_plot = cluster.df_cluster_for_plot
 
@@ -69,11 +145,11 @@ def app():
                                             # line=dict(color='#00AF64'), opacity=0.8,
                                             name='Исходные данные'))
 
-            fig.add_trace(go.Scatter(x=df_plot.iloc[:,2], y=df_plot.iloc[:,3],
-                                     # line=dict(color='#CF2817'),
-                                     mode='markers',
-                                     marker=dict(color='#ff0000', size=age_fig2, symbol = 'circle'),
-                                     name='Результаты кластеризации'))
+            # fig.add_trace(go.Scatter(x=df_plot.iloc[:,2], y=df_plot.iloc[:,3],
+            #                          # line=dict(color='#CF2817'),
+            #                          mode='markers',
+            #                          marker=dict(color='#ff0000', size=age_fig2, symbol = 'circle'),
+            #                          name='Результаты кластеризации'))
 
             fig.update_layout(legend_orientation="h",
                               legend=dict(x=.5, xanchor="center"),
@@ -83,12 +159,14 @@ def app():
                               margin=dict(l=20, r=0, t=0, b=0))
             st.write(fig)
 
+
             # Загрузка данных результата
             st.subheader('Скачайте файл результата в Excel')
             st.markdown(get_table_download_link(df_initial=df_initial,
                                                 df_new=df_new,
                                                 df_cluster_for_plot=df_plot,
                                                 df_agg=cluster.df_agg), unsafe_allow_html=True)
+
             st.caption('Автор: Капанский А.А., 2021 г.')
         except Exception as err:
             st.subheader('Что-то пошло не так. \n Ошибка:')
